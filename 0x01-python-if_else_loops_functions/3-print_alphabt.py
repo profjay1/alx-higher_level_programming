337b1d20d0ef68f2017965b34209b7b6dbd557a6
root <jibstechnology@gmail.com>
2022-09-20 14:59:14 -0700

    Fixing error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if (i == ord('e') or i == ord('q')):
-        i += 1
-    else:
-        print(chr(i), end="")
-        i += 1
+for i in range(ord("a"), ord("z")+1):
+    if i == ord("q") or i == ord("e"):
+        continue
+
+    print("{:c}".format(i), end="")
