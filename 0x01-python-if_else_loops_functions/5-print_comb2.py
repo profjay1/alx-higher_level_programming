f4bbf31dd0e7087f7854480e80a0a0545c81fdd5
root <jibstechnology@gmail.com>
2022-09-20 15:21:48 -0700

    Fixing errors

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{str(i).zfill(2)}, ", end="")
-print("99")
+for i in range(100):
+    print("{:d}".format(i).rjust(2, "0"),
+          end="\n" if i == 99 else ", ")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        if (ord('a') <= ord(char) and ord(char) <= ord('z')):
-            char = chr(ord(char) - 32)
-        print(char, end="")
-    print("\n", end="")
+    print("".join(["{:c}".format(ord(c)-32)
+          if ord(c) in range(97, 123) else c
+          for c in str]))
