8187c169659c7a1faa56a7a973f3460175d3257c
root <jibstechnology@gmail.com>
2022-09-22 14:55:23 -0700

    Fixing error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
-if __name__ == "__main__":
-    from add_0 import add
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == '__main__':
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
