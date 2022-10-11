221c17bb630b013651412b98e7c2e3f8c09118ec
profjay1 <jibstechnology@gmail.com>
2022-10-11 13:54:54 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Square Class without methods or public attributes"""
+
+    def __init__(self, size):
+        """Instantiation"""
+        self.__size = size
