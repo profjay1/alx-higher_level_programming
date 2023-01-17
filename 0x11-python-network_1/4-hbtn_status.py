6c074c31c7324d3c9b8fa915dea841a6dacf2ea1
profjay1 <jibstechnology@gmail.com>
2023-01-17 23:16:57 +0100

    pthon_network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
